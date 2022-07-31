c4ae165efb11a724e9d0a2925a7efff8378c95b2
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-07-31 14:17:47 -0400

    implemented a link to the post page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Header from "../components/Header";
 import {sanityClient, urlFor } from '../sanity';
 import {Post} from "../typings";
+import Link from "next/link";
 
 interface Props {
     posts: [Post]
@@ -28,6 +29,27 @@ export default function Home({posts}: Props) {
 
             <img className='hidden md:inline-flex h-32 lg:h-full' src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt=""/>
         </div>
+
+        {/*Posts*/}
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap03 md:gap-6 p-2 md:p-6'>
+            {posts.map(post => (
+                <Link key={post._id} href={`/post/${post.slug.current}`}>
+                    <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+                        {post.mainImage && (
+                            <img className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out' src={urlFor(post.mainImage).url()!} alt=''/>
+                        )}
+                        <div className='flex justify-between p-5 bg-white'>
+                            <div>
+                                <p className='text-lg font-bold'>{post.title}</p>
+                                <p className='text-xs'>{post.description} by {post.author.name}</p>
+                            </div>
+
+                            <img className='h-12 w-12 rounded-full' src={urlFor(post.author.image).url()!} alt=''/>
+                        </div>
+                    </div>
+                </Link>
+            ))}
+        </div>
     </div>
   )
 }
@@ -36,13 +58,14 @@ export default function Home({posts}: Props) {
 export const getServerSideProps = async () => {
  const query = `*[_type == 'post']{
         _id,
-            title,
-            slug,
-            description,
-            author -> {
-                name,
-                    image
-            }
+        title,
+        slug,
+        description,
+        mainImage,
+        author -> {
+            name,
+            image
+        }
     }`;
 
  const posts = await sanityClient.fetch(query);
