d18ccd9c81abc656fa3675ee9fba79f2e76c8f14
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-07-31 03:21:02 -0400

    added sanity to next app and implemented server-side rendering in home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
+import {sanityClient, urlFor } from '../sanity';
+import {Post} from "../typings";
 
-const Home: NextPage = () => {
+interface Props {
+    posts: [Post]
+}
+
+export default function Home({posts}: Props) {
+    console.log(posts);
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -25,4 +32,24 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+
+export const getServerSideProps = async () => {
+ const query = `*[_type == 'post']{
+        _id,
+            title,
+            slug,
+            description,
+            author -> {
+                name,
+                    image
+            }
+    }`;
+
+ const posts = await sanityClient.fetch(query);
+
+ return {
+     props: {
+         posts
+     }
+ }
+}
